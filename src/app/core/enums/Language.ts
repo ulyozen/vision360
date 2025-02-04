229d34c9c6511e8f501e5de1ea63f13c9ef33904
Baurzhan <ulyozen@gmail.com>
2025-02-05 00:52:34 +0500

    feat: added enums

---
+++ src/app/core/enums/Language.ts
@@ -0,0 +1,4 @@
+export enum LanguageEnum {
+  English = 'en',
+  Polish = 'pl',
+}
---
+++ src/app/core/enums/StorageKeys.ts
@@ -0,0 +1,5 @@
+export enum StorageKeys {
+  Theme = 'theme',
+  Language = 'language',
+  IsAuthenticated = 'isAuthenticated',
+}
