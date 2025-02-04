00e718b90d9655e8495c505b309817d8d8345240
Baurzhan <ulyozen@gmail.com>
2025-02-05 00:50:13 +0500

    feat: added core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderModule } from './components/header/header.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HeaderModule
+  ],
+  exports: [
+    HeaderModule
+  ]
+})
+export class CoreModule {}
