f48e4b5a18a234ede74e48df1a81ec0c6d1fa9f4
Baurzhan <ulyozen@gmail.com>
2025-02-05 19:28:38 +0500

    refactor(core): migrate to NgRx and improve state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {HeaderService} from './core/services/header.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AppService } from './core/services/app.service';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+
+import { selectIsAuthenticated, selectLanguage, selectTheme } from './core/store/header/header.selectors';
+import { LanguageEnum, ThemeEnum } from './core/enums';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false
 })
-export class AppComponent implements OnInit {
-  constructor(private headerService: HeaderService) {}
+export class AppComponent implements OnInit, OnDestroy {
+  theme$: Observable<ThemeEnum>
+  language$: Observable<LanguageEnum>
+  isAuthenticated$: Observable<boolean>
+
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private store: Store,
+    private app: AppService
+  ) {
+    this.theme$ = this.store.select(selectTheme)
+    this.language$ = this.store.select(selectLanguage)
+    this.isAuthenticated$ = this.store.select(selectIsAuthenticated)
+  }
 
   ngOnInit(): void {
-    this.headerService.loadTheme()
-    this.headerService.loadLanguage()
-    this.headerService.loadAuthentication()
+    this.subscriptions.add(
+      this.language$.subscribe(language => this.app.applyLanguage(language))
+    )
+    this.subscriptions.add(
+      this.theme$.subscribe(theme => this.app.applyTheme(theme))
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe()
   }
 }
