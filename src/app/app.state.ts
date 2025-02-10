7bb4535bd3b7200072e3dfa0b90e30cbf5a4c872
Baurzhan <ulyozen@gmail.com>
2025-02-10 13:31:47 +0500

    feat: added `auth` state to `AppState`

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,5 +1,7 @@
 import { HeaderState } from './core/store/header/header.state';
+import { AuthState } from './features/auth/store/auth.state';
 
 export interface AppState {
   header: HeaderState
+  auth: AuthState
 }
