643d12f44794dbdf2881b069139abccba7e86dfa
Baurzhan <ulyozen@gmail.com>
2025-02-11 03:10:08 +0500

    feat: added error handling and token storage in `onSubmit`

--- src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -5,6 +5,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { selectMessage } from '../../store/auth.selectors';
 import { cleanAuthMessage } from '../../store/auth.actions';
 import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { User, UserResponse } from '../../models/User';
+import { setAuthenticated } from '../../../../core/store/header/header.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -20,12 +24,15 @@ export class SignInComponent implements OnInit, OnDestroy {
 
   constructor(
     private store: Store,
+    private router: Router,
     private fb: FormBuilder,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
   ) {
     this.signInForm = this.fb.group({
-      email: ['', Validators.required, Validators.email],
-      password: ['', Validators.required, Validators.minLength(3)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(1)]],
+      rememberMe: [false]
     })
   }
 
@@ -39,6 +46,34 @@ export class SignInComponent implements OnInit, OnDestroy {
   }
 
   onSubmit() {
+    if (this.signInForm.invalid) return
+
+    const user = this.signInForm.value as User
+
+    this.authService.signin(user).subscribe((response: UserResponse) => {
+      const { success, errors, token } = response
+
+      const email = this.signInForm.get('email')
+      const password = this.signInForm.get('password')
+
+      if (errors?.includes(`Email ${email!.value} does not exist.`))
+        email!.setErrors({ userExists: true })
+
+      if (errors?.includes('Incorrect password.'))
+        password!.setErrors({ incorrect: true })
+
+      if (!success || !token) return
+
+      const { accessToken, expiresIn } = token
+      localStorage.setItem('accessToken', accessToken)
+      localStorage.setItem('tokenExpiration', (Date.now() + expiresIn * 1000).toString())
+
+      this.store.dispatch(setAuthenticated())
+      this.router.navigate(['/'])
+    })
+  }
+
+  onForgotPassword() {
 
   }
 
@@ -60,6 +95,4 @@ export class SignInComponent implements OnInit, OnDestroy {
         }, duration)
     })
   }
-
-
 }
