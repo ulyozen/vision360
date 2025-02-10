4d930f70078bf3c9b8fb0483392859a95055c888
Baurzhan <ulyozen@gmail.com>
2025-02-10 13:32:38 +0500

    feat: added `auth` reducer to `appReducers`

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,7 +1,9 @@
-import {ActionReducerMap} from '@ngrx/store';
-import {AppState} from './app.state';
-import {headerReducer} from './core/store/header/header.reducer';
+import { ActionReducerMap } from '@ngrx/store';
+import { AppState } from './app.state';
+import { headerReducer } from './core/store/header/header.reducer';
+import { authReducer } from './features/auth/store/auth.reducer';
 
 export const appReducers: ActionReducerMap<AppState> = {
   header: headerReducer,
+  auth: authReducer,
 }
