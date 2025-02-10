2ed1b6f25bdac338497d56cc244f34a72f993804
Baurzhan <ulyozen@gmail.com>
2025-02-10 13:27:06 +0500

    feat: added routing configuration to `AuthRoutingModule`

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-
+const routes: Routes = [
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AuthRoutingModule { }
