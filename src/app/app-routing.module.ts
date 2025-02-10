3d43fc7c93941f62e61ddcf033d54f4b873b6d99
Baurzhan <ulyozen@gmail.com>
2025-02-10 13:30:54 +0500

    feat: added routes to `AppRoutingModule`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './core/components/home/home.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule)},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
