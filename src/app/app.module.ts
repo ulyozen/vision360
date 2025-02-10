c00071453443174dcbb2ac405a0a40b8da516280
Baurzhan <ulyozen@gmail.com>
2025-02-11 03:00:09 +0500

    feat: added `authInterceptor` to `HttpClient`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { SharedModule } from './shared/shared.module';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StoreModule } from '@ngrx/store';
-import { appReducers } from './app.reducers';
-import { CoreModule } from './core/core.module';
-import { provideHttpClient } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
+
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { appTranslate } from './app.translate';
+import { appReducers } from './app.reducers';
 import { FeaturesModule } from './features/features.module';
+import { authInterceptor } from './features/auth/interceptor/auth.interceptor';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { FeaturesModule } from './features/features.module';
   ],
   providers: [
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
   bootstrap: [AppComponent]
 })
