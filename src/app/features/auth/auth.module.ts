d122c79f6a91c327657e04e83f6e2f49b1d3d2f5
Baurzhan <ulyozen@gmail.com>
2025-02-10 13:26:12 +0500

    feat: updated `AuthModule` to include components and modules

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIcon,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
