10b18eeb00282283b27f8e69181070aaac01e49c
Baurzhan <ulyozen@gmail.com>
2025-02-11 22:30:32 +0500

    feat: added `StoreDevtoolsModule` and removed `StoreModule.forRoot`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { appReducers } from './app.reducers';
 import { FeaturesModule } from './features/features.module';
 import { authInterceptor } from './features/auth/interceptor/auth.interceptor';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -27,7 +29,10 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     NgbModule,
     StoreModule.forRoot(appReducers),
-    TranslateModule.forRoot(appTranslate)
+    TranslateModule.forRoot(appTranslate),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+    })
   ],
   providers: [
     provideAnimationsAsync(),
